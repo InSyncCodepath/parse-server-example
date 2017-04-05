4fda9ad9f04efd0731d92ee24e1f48874d38d069
Unum Sarfraz <anamsarfraz@gmail.com>
2017-04-05 01:53:21 -0700

    comment out installation id

--- cloud/main.js
+++ cloud/main.js
@@ -40,7 +40,7 @@ Parse.Cloud.define('pushChannelTest', function(request, response) {
   // use to custom tweak whatever payload you wish to send
   var pushQuery = new Parse.Query(Parse.Installation);
   pushQuery.equalTo("deviceType", "android");
-  pushQuery.equalTo("installationId", "4cdce28b-3099-4476-9268-f60660d25c48");
+  //pushQuery.equalTo("installationId", "4cdce28b-3099-4476-9268-f60660d25c48");
 
   var payload = {};
 
