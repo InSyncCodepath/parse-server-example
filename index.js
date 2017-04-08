0063b9013d0aec575906a25944e300a6943774e9
Unum Sarfraz <anamsarfraz@gmail.com>
2017-04-08 12:08:26 -0700

    Updated mongodb uri

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ if (process.env.S3_ENABLE) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://anamsarfraz:Lilonstitch2@ds119588.mlab.com:19588/heroku_lcm7nq9p',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
