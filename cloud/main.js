abfb85c075a574c7aa201a527f2570fa035afd8c
Unum Sarfraz <anamsarfraz@gmail.com>
2017-04-15 20:10:42 -0700

    added background jobs for rsvp and event end

--- cloud/main.js
+++ cloud/main.js
@@ -131,3 +131,118 @@ Parse.Cloud.define("iosPushTest", function(request, response) {
   response.success('success');
 });
 
+// Event expiration time job
+Parse.Cloud.define("EventEndJob", function(request, response) {
+
+  var queryEnded = new Parse.Query(Parse.Event);
+  queryEnded.equalTo("hasEnded", true);
+
+  var bufferDate = Date();
+  var bufferHours = 3;
+  bufferDate.setHours(bufferDate.getHours() - bufferHours);
+  var queryTime = new Parse.Query(Parse.Event);
+  queryTime.lessThan("endDate", bufferDate);
+
+  var endEventQuery = Parse.Query.or(queryEnded, queryTime);                                                                                                                                            
+  endEventQuery.find({
+    success: function(events) {	
+	var eventsToCheck = [];
+	var index = 0;
+	for (var i = 0; i < events.length; i++) {
+          var highlightsVideo = events[i].highlightsVideo;
+	  if (highlightsVideo != null && highlightsVideo.trim().length) {
+            continue;
+          } 
+	  
+	  eventsToCheck[index] = events[i];
+	  index++;
+	 }
+	 
+         var eventHostQuery = new Parse.Query(Parse.UserEventRelation);
+         eventHostQuery.containedIn("event", eventsToCheck); 
+         eventHostQuery.equalTo("isHosting", true);
+         eventHostQuery.find({
+             success: function(eventHosts) {
+               var userIds = [];
+               var userIndex = 0;
+               for (var i = 0; i < eventsHosts.length; i++) {
+                 userIds[index] = eventsHosts[i].userId;
+                 userIndex++;              
+               }
+	       var pushQuery = new Parse.Query(Parse.Installation);
+	       pushQuery.equalTo("deviceType", "android");
+	       pushQuery.containedIn("userId", userIds);
+
+	       Parse.Push.send({
+		 where: pushQuery, // Set our Installation query                                                                                                                                                              
+		   data: {
+		     eventdata: eventHosts
+		   }
+	       }, { success: function() {
+		      console.log("#### PUSH OK");
+	       }, error: function(error) {
+		      console.log("#### PUSH ERROR" + error.message);
+	       }, useMasterKey: true});
+
+	       response.success('success');
+             },
+             error: function() {
+                response.error("Error fetching host user ids for event end processing");
+             }
+         });     
+    },
+    error: function() {
+	response.error("Error fetching ended events to generate highlights.");
+    }
+  });  
+});
+
+// RSVP Status Check Job
+Parse.Cloud.define("RSVPStatusJob", function(request, response) {
+  var currentDate = Date();
+  var eventQuery = new Parse.Query(Parse.Event);
+  eventQuery.equalTo("hasEnded", false);
+  eventQuery.lessThan("startDate", currentDate);
+
+  eventQuery.find({
+    success: function(events) {	
+         var eventHostQuery = new Parse.Query(Parse.UserEventRelation);
+         eventHostQuery.containedIn("event", events); 
+         eventHostQuery.equalTo("isHosting", false);
+         eventHostQuery.lessThan("rsvpStatus", 2);
+         eventHostQuery.find({
+             success: function(eventHosts) {
+               var userIds = [];
+               var userIndex = 0;
+               for (var i = 0; i < eventsHosts.length; i++) {
+                 userIds[index] = eventsHosts[i].userId;
+                 userIndex++;              
+               }
+	       var pushQuery = new Parse.Query(Parse.Installation);
+	       pushQuery.equalTo("deviceType", "android");
+	       pushQuery.containedIn("userId", userIds);
+
+	       Parse.Push.send({
+		 where: pushQuery, // Set our Installation query                                                                                                                                                              
+		   data: {
+		     guestdata: eventHosts
+		   }
+	       }, { success: function() {
+		      console.log("#### PUSH OK");
+	       }, error: function(error) {
+		      console.log("#### PUSH ERROR" + error.message);
+	       }, useMasterKey: true});
+
+	       response.success('success');
+             },
+             error: function() {
+                response.error("Error fetching host user ids for rsvp status");
+             }
+         });     
+    },
+    error: function() {
+	response.error("Error fetching current events to check rsvp status.");
+    }
+  });  
+});
+
